e9d24cf93893ba5c231cb41aea5ba9de42329181
elinnilsson <elinhejnilsson@gmail.com>
2017-09-20 18:40:03 +0200

    First messy commit.

---
+++ check-list.js
@@ -0,0 +1,127 @@
+window.checklistData = [
+    {
+        "description": "Balkong/uteplats",
+        "meetsRequirements": false,
+        "comment": ""
+    },
+    {
+        "description": "Avgift",
+        "meetsRequirements": false,
+        "comment": ""
+    },
+    {
+        "description": "Stambytt",
+        "meetsRequirements": false,
+        "comment": ""
+    },
+    {
+        "description": "Våningsplan",
+        "meetsRequirements": false,
+        "comment": ""
+    },
+    {
+        "description": "Spis och ugn",
+        "meetsRequirements": false,
+        "comment": ""
+    },
+    {
+        "description": "Förråd",
+        "meetsRequirements": false,
+        "comment": ""
+    },
+    {
+        "description": "Parkering",
+        "meetsRequirements": false,
+        "comment": ""
+    },
+    {
+        "description": "Porttelefon",
+        "meetsRequirements": false,
+        "comment": ""
+    },
+    {
+        "description": "Badrum",
+        "meetsRequirements": false,
+        "comment": ""
+    },
+    {
+        "description": "Kök",
+        "meetsRequirements": false,
+        "comment": ""
+    },
+    {
+        "description": "Vitvaror",
+        "meetsRequirements": false,
+        "comment": ""
+    },
+    {
+        "description": "Avstånd till centrum",
+        "meetsRequirements": false,
+        "comment": ""
+    },
+    {
+        "description": "Avstånd till jobb/studier",
+        "meetsRequirements": false,
+        "comment": ""
+    },
+    {
+        "description": "Avstånd till mataffär",
+        "meetsRequirements": false,
+        "comment": ""
+    },
+    {
+        "description": "Boarea",
+        "meetsRequirements": false,
+        "comment": ""
+    },
+    {
+        "description": "Pris",
+        "meetsRequirements": false,
+        "comment": ""
+    },
+    {
+        "description": "Föreningens skuldsättning",
+        "meetsRequirements": false,
+        "comment": ""
+    },
+    {
+        "description": "Hiss",
+        "meetsRequirements": false,
+        "comment": ""
+    },
+    {
+        "description": "Ytskikt",
+        "meetsRequirements": false,
+        "comment": ""
+    },
+    {
+        "description": "Tv",
+        "meetsRequirements": false,
+        "comment": ""
+    },
+    {
+        "description": "Internet",
+        "meetsRequirements": false,
+        "comment": ""
+    },
+    {
+        "description": "Planlösning",
+        "meetsRequirements": false,
+        "comment": ""
+    },
+    {
+        "description": "Fönster (3 glas?)",
+        "meetsRequirements": false,
+        "comment": ""
+    },
+    {
+        "description": "El",
+        "meetsRequirements": false,
+        "comment": ""
+    },
+    {
+        "description": "Ventilation",
+        "meetsRequirements": false,
+        "comment": ""
+    },
+];
---
+++ script.js
@@ -0,0 +1,97 @@
+(function(){
+
+    var defaultCheckListRef = firebase.database().ref('default-checklist');
+    defaultCheckListRef.once('value', function(snapshot) {
+        document.querySelector('.content').innerHTML = tmpl('checklist', snapshot.val());
+        
+        
+        
+        
+        document.querySelectorAll('.js-show-comment').forEach((element) => {
+                element.addEventListener('click', (e) => {
+                let commentInput = e.target.parentElement.querySelector('.js-comment');
+                commentInput.classList.remove('hidden');
+                e.target.className += ' hidden';
+                e.target.parentElement.querySelector('.js-hide-comment').classList.remove('hidden');
+            });
+        });
+
+        document.querySelectorAll('.js-hide-comment').forEach((element) => {
+                element.addEventListener('click', (e) => {
+                let commentInput = e.target.parentElement.querySelector('.js-comment');
+                commentInput.className += ' hidden';
+                e.target.className += ' hidden';
+                e.target.parentElement.querySelector('.js-show-comment').classList.remove('hidden');
+            });
+        });
+
+        document.querySelector('.js-save-checklist').addEventListener('click', () => {
+            let checklist = collectData();
+            writeUserChecklist(firebase.auth().currentUser.uid, checklist);
+        });
+
+        function collectData(){
+            let checklist = {
+                name: document.querySelector('.js-checklist-name').value,
+                checkItems: []
+            };
+            document.querySelectorAll('.js-list-item').forEach((element) => {
+                let listItem = {};
+                listItem.description = element.querySelector('.js-description').innerHTML;
+                listItem.meetsRequirements = element.querySelector('.js-meets-requirements').value === 'on';
+                listItem.comment = element.querySelector('.js-comment').value;
+                checklist.checkItems.push(listItem);
+            });
+
+            return checklist;
+        }
+        
+        function writeUserChecklist(userId, checklist) {
+          var key = firebase.database()
+                               .ref('users/' + userId + '/checklists/')
+                               .push()
+                               .key;
+          firebase.database()
+                  .ref('users/' + userId + '/checklists/' + key)
+                  .set(checklist)
+                  .then(() => {
+                    alert('Väl sparat min vän!');
+                  })
+                  .catch(() => {
+                    console.log('uh-oh! nu blev det fel');
+                  });
+        }
+        
+        let userId = firebase.auth().currentUser.uid;
+        let defaultChecklistListRef = firebase.database().ref('users/' + userId + '/checklists');
+        defaultChecklistListRef.on('value', function(snapshot) {
+            let checklists = snapshot.val();
+            let templateFriendlyChecklists = [];
+            for(var i in checklists){
+                if(checklists.hasOwnProperty(i)){
+                    checklists[i].id = i;
+                    templateFriendlyChecklists.push(checklists[i]);
+                }
+            }
+           
+            document.querySelector('.checklist-list').innerHTML = tmpl('checklist-list', templateFriendlyChecklists);
+            
+            let deleteButtons = document.querySelector('.js-delete-checklist');
+                if(deleteButtons){
+                    deleteButtons.addEventListener('click', (e) => {
+                    let id = e.target.dataset.checklistId;
+                    let checklistToBeDeletedRef = firebase.database().ref('users/' + userId + '/checklists/' + id);
+                    checklistToBeDeletedRef.remove();
+                });
+            }
+        });
+    });
+
+    document.querySelector('.signout').addEventListener('click', () => {
+        firebase.auth().signOut().then(function() {
+          // Sign-out successful.
+        }).catch(function(error) {
+          // An error happened.
+        });
+    });
+})();
